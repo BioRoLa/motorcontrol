9a7b031c8f97175ba22ade874bc141af3e4f6c55
changtsailin <angel.tl314@gmail.com>
2026-04-03 22:11:13 +0800

    fix: enable ReceiveFifoLocked to prevent message loss in 3-motor (ABAD) setup

--- Core/Inc/version_info.h
+++ Core/Inc/version_info.h
@@ -8,10 +8,10 @@
 #ifndef VERSION_INFO_H_
 #define VERSION_INFO_H_
 
-#define FIRMWARE_VERSION   "5.2.5"
+#define FIRMWARE_VERSION   "5.2.6"
 #define FIRMWARE_DATE      __DATE__
 #define FIRMWARE_TIME      __TIME__
-#define AUTHOR_NAME        "Yi-Syuan, Shen (r12522823)"
+#define AUTHOR_NAME        "Shi-Cheng, Lin (r14522837)"
 
 #define MODIFICATION_INFO  "Stable Version."
 
