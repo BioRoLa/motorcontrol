e514dcd5461c0d08ecbd42c8426dd7cfbf456ea5
ac019 <alex.chen019@gmail.com>
2026-06-16 15:59:26 +0800

    update verison number

--- Core/Inc/version_info.h
+++ Core/Inc/version_info.h
@@ -8,7 +8,7 @@
 #ifndef VERSION_INFO_H_
 #define VERSION_INFO_H_
 
-#define FIRMWARE_VERSION   "5.3.0"
+#define FIRMWARE_VERSION   "5.3.1"
 #define FIRMWARE_DATE      __DATE__
 #define FIRMWARE_TIME      __TIME__
 #define AUTHOR_NAME        "Chen, Alex (R14522850)"
